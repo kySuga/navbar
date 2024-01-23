550337b481bc36a483d8f8130e6ffe08cf53f51d
kySuga <kyle@masuga.com>
2024-01-22 21:24:09 -0500

    Create navbar based on JS course. Styles and JS are different from project.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
-// classList - shows/gets all classes
-// contains - checks classList for specific class
-// add - add class
-// remove - remove class
-// toggle - toggles class
+// classList (property) - shows/gets all classes
+// contains (method) - checks classList for specific class
+// add (method) - add class
+// remove (method) - remove class
+// toggle (method) - toggles class
+
+const navMobileToggle = document.querySelector(".nav__mobile-toggle");
+const navMain = document.querySelector(".nav__main");
+
+// setup slightly different from John's setup since I'm targeting the button for the action
+// Also have styles setup different as well
+navMobileToggle.addEventListener("click", function() {
+  if (navMobileToggle.classList.contains("nav__mobile-toggle__active")) {
+    navMain.classList.remove("nav__mobile-nav__show");
+    navMobileToggle.classList.remove("nav__mobile-toggle__active");
+  } else if (!navMobileToggle.classList.contains("nav__mobile-toggle__active")) {
+    navMain.classList.add("nav__mobile-nav__show");
+    navMobileToggle.classList.add("nav__mobile-toggle__active");
+  }
+});
+
